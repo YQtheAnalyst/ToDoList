8eb5be2551bbe9c4adc1ce9e7a4fca1fa09adfd9
YQtheAnalyst <yiqingli07@gmail.com>
2024-06-02 12:02:53 +0100

    create dummy templates

--- todolist/tasksapp/views.py
+++ todolist/tasksapp/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(response):
-    return HttpResponse("Hello World, I am genius!")
+def index(request):
+    return render(request, 'tasksapp/list.html')

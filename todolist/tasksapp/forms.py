970dbe0695d196e84e90ebbc765b8c08645543f5
YQtheAnalyst <yiqingli07@gmail.com>
2024-06-08 21:22:32 +0100

    todo: add placeholder

--- todolist/tasksapp/forms.py
+++ todolist/tasksapp/forms.py
@@ -3,6 +3,7 @@ from django.forms import ModelForm
 from .models import *
 
 class TaskForm(forms.ModelForm):
+    # title = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Add new task...'})) something is wrong
 
     class Meta:
         model = task # what model are we trying to create the form

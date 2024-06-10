b2b5ba70224d2d658bafd004b3646f23052b0782
YQtheAnalyst <yiqingli07@gmail.com>
2024-06-10 20:01:22 +0100

    setup simple-jwt based on the documentation

--- todolist/tasksapp/api/views.py
+++ todolist/tasksapp/api/views.py
@@ -1,6 +1,12 @@
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+# Django REST framework JSON Web Token Authentication - Simple JWT
+# What is the difference? Why choose this?
 
 
+@api_view(['GET']) # this takes http method we can set
 def getRoutes(request):
 
     # show all the endpoints
@@ -9,4 +15,4 @@ def getRoutes(request):
         '/api/token/refresh',
     ]
 
-    return JsonResponse(routes, safe=False)
+    return Response(routes)

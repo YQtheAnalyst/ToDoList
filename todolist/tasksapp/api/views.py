c4119738976fa536f142c381a181d910e2f49f89
YQtheAnalyst <yiqingli07@gmail.com>
2024-06-10 20:57:04 +0100

    customize the variable from tokens - rewrite builtt_in views

--- todolist/tasksapp/api/views.py
+++ todolist/tasksapp/api/views.py
@@ -2,8 +2,27 @@ from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 # Django REST framework JSON Web Token Authentication - Simple JWT
 # What is the difference? Why choose this?
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 
 
 @api_view(['GET']) # this takes http method we can set
